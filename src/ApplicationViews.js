cd57a078384febed9142b7e668c3b562cfb0ef61
sxiong0519 <sxiong0519@gmail.com>
2021-07-23 14:02:04 -0500

    created navbar and able to display homepage

---
+++ src/ApplicationViews.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { Route } from "react-router-dom"
+import { Home } from "./Home"
+
+
+export const ApplicationViews = () => {
+    return (
+        <>
+            <Route exact path="/">
+                <Home />
+            </Route>
+        </>
+    )
+}
--- src/AuTrack.js
+++ src/AuTrack.js
@@ -1,33 +1,33 @@
-import React from "react"
-import { Route, Redirect } from "react-router-dom"
-import { ApplicationViews } from "./ApplicationViews"
-import { NavBar } from "./nav/NavBar"
-import { Login } from "./auth/Login"
-import { Register } from "./auth/Register"
-import "./AuTrack.css"
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+import { ApplicationViews } from "./ApplicationViews";
+import { Login } from "./components/auth/Login";
+import { Register } from "./components/auth/Register";
+import "./AuTrack.css";
+import { NavHome } from "./components/nav/NavHome";
 
 export const AuTrack = () => (
   <>
-    <Route
+  <Route
       render={() => {
-        if (sessionStorage.getItem("autrack_user")) {
+        if (localStorage.getItem("autrack_user")) {
           return (
             <>
-              <NavBar />
+              <NavHome />
               <ApplicationViews />
             </>
-          )
+          );
         } else {
           return <Redirect to="/login" />;
         }
       }}
     />
 
-    <Route path="/login">
+  <Route path="/login">
       <Login />
-    </Route>
-    <Route path="/register">
+  </Route>
+  <Route path="/register">
       <Register />
-    </Route>
+  </Route>  
   </>
 )
