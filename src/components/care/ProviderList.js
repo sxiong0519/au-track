7a3c1c43c1688d929b565ecff4e2423c2a2dd499
sxiong0519 <sxiong0519@gmail.com>
2021-07-27 10:26:57 -0500

    created card previews

--- src/components/care/ProviderList.js
+++ src/components/care/ProviderList.js
@@ -1,17 +1,26 @@
-import React, {useContext, useEffect} from "react";
-import { useHistory } from "react-router-dom";
+import React, {useContext, useEffect, useState} from "react";
+import { useHistory, useParams } from "react-router-dom";
 import { ProviderContext } from "./ProviderProvider";
 import { ProviderCard } from "./ProviderCard";
+import { ChildContext } from "../children/ChildProvider";
 
 export const ProviderList = () => {
 
     const { providers, getProviders } = useContext(ProviderContext)
+    const { getChildById } = useContext(ChildContext)
+    const [child, setChild] = useState({})
+
     const history = useHistory()
 
+    const { childId } = useParams();
 
     useEffect(() => {
         console.log("LocationList: useEffect - getProviders")
-        getProviders()
+        getChildById(childId)
+        .then(getProviders())
+        .then((response) => {
+          setChild(response)
+        })
     }, [])
 
     return (
@@ -21,7 +30,8 @@ export const ProviderList = () => {
         {console.log("ProviderList: Render", providers)}
           {
              providers.map(provider => {
-              return <ProviderCard key={provider.id} provider={provider} />
+                  if(provider.childId === child.id){
+              return <ProviderCard key={provider.id} provider={provider} />}
             })
           }
           
--- src/components/nav/NavChild.js
+++ src/components/nav/NavChild.js
@@ -1,8 +1,24 @@
-import React from "react"
-import { Link } from "react-router-dom"
+import React, { useContext, useEffect, useState } from "react"
+import { Link, useParams } from "react-router-dom"
+import { ChildContext } from "../children/ChildProvider"
 import "./Nav.css"
 
 export const NavChild = (props) => {
+    const { getChildById } = useContext(ChildContext)
+    const [child, setChild] = useState({})
+    const {childId} = useParams();
+
+    useEffect(() => {
+        console.log("useEffect", childId)
+        getChildById(childId)
+        .then((response) => {
+            setChild(response)
+        })
+    }, [])
+
+
+
+
     return (
         <ul className="navbar">
             <li className="navbar__item active">
@@ -10,11 +26,11 @@ export const NavChild = (props) => {
             </li>
             
             <li className="navbar__item">
-                <Link className="navbar__link" to="/milestones">Milestones </Link>
+                <Link className="navbar__link" to={`/milestones/list/${child.id}`}>Milestones </Link>
             </li>
             
             <li className="navbar__item">
-                <Link className="navbar__link" to="/providers">Provider</Link>
+                <Link className="navbar__link" to={`/providers/list/${child.id}`}>Provider</Link>
             </li>
         </ul>
     )
