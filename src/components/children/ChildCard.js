d68457bd8eb4364c82b7f281b39432a9450b4bfb
sxiong0519 <sxiong0519@gmail.com>
2021-07-28 11:28:09 -0500

    CRUD functionality for children

--- src/components/children/ChildCard.js
+++ src/components/children/ChildCard.js
@@ -1,12 +1,20 @@
-import React from "react"
-import { Link } from "react-router-dom"
+import React, { useContext } from "react"
+import { Link, useHistory } from "react-router-dom"
 import "./Child.css"
+import { ChildContext } from "./ChildProvider"
 
 
 export const ChildCard = ({ child }) => {
-    
+    const { deleteChild } = useContext(ChildContext)
+    const history = useHistory();
+
+    const childDelete = () => {
+        deleteChild(child.id)
+    }
+
     return (
         <>
+        <center>
             <section className="child_card">
                 <div className="child"><h3>{child.name}</h3>
                     <div className="childimg">
@@ -14,7 +22,15 @@ export const ChildCard = ({ child }) => {
                         </Link>
                     </div>    
                 </div>
+                {child.parentId === parseInt(localStorage.getItem("autrack_user")) ? 
+                    <section className="buttons">
+                    <button className="btns" onClick={childDelete}>Delete</button>
+                    <button className="btns" onClick={() => {
+                        history.push(`/children/edit/${child.id}`)
+                            }}>Edit</button>
+                    </section> : ""}
             </section>
+        </center>
         </>
 )}
 
---
+++ src/components/children/ChildForm.js
@@ -0,0 +1,95 @@
+import React, { useContext, useEffect, useState } from "react"
+import { useHistory, useParams } from 'react-router-dom';
+import "./Child.css"
+import { ChildContext } from "./ChildProvider";
+
+
+
+export const ChildForm = () => {
+  const { addChildren, getChildById, updateChild } = useContext(ChildContext)
+  
+  const [isLoading, setIsLoading] = useState(true);
+
+  const [child, setChild] = useState({})
+
+  const { childId } = useParams();
+  const history = useHistory();
+
+
+
+  //when a field changes, update state. The return will re-render and display based on the values in state
+  //Controlled component
+  const handleControlledInputChange = (event) => {
+    /* When changing a state object or array,
+    always create a copy, make changes, and then set state.*/
+    const newChild = { ...child }
+    /* Animal is an object with properties.
+    Set the property to the new value
+    using object bracket notation. */
+    newChild[event.target.id] = event.target.value
+    // update state
+    setChild(newChild)
+  }
+
+  const handleClickSaveChild = () => {
+    if (child.name === undefined || child.image === undefined) {
+        window.alert("Please complete the form")
+    } else if (childId) {
+        updateChild({
+            id: child.id, 
+            name: child.name,
+            image: child.image,
+            parentId: parseInt(localStorage.getItem("autrack_user"))
+        })
+        .then(() => history.push("/"))
+    } else {
+        const newChild = {
+            name: child.name,
+            image: child.image,
+            parentId: parseInt(localStorage.getItem("autrack_user"))
+      }
+      addChildren(newChild)
+        .then(() => history.push("/"))
+      }
+    }
+
+  useEffect(() => {
+    if (childId) {
+        getChildById(childId)
+            .then(child => {
+                setChild(child)
+                setIsLoading(false)
+            })
+    } else {
+        setIsLoading(false)
+    }
+}, [])
+
+
+return (
+        <form className="childForm">
+            <h1 className="childForm__title child_header">{childId ? "Update Child" : "New Child"}</h1>
+            <fieldset>
+                <div className="form-group">
+                    <label htmlFor="name">Name:</label>
+                    <input type="text" id="name" required autoFocus className="form-control" placeholder="Enter child's name" value={child.name} onChange={handleControlledInputChange} />
+                </div>
+            </fieldset>
+            <fieldset>
+                <div className="form-group">
+                    <label htmlFor="image">Image:</label>
+                    <input type="text" id="image" required autoFocus className="form-control" placeholder="Upload an image" value={child.image} onChange={handleControlledInputChange} />
+                </div>
+            </fieldset>
+            <div className="buttons"><button className="btns" disabled={isLoading} onClick={
+                (event) => {
+                    event.preventDefault()
+                    handleClickSaveChild()
+                }
+            }>
+                 {childId ? "Update child" : "Save child"}
+            </button>{childId ? <button className="btns" onClick={() => history.push("/children")}>Cancel</button> :<button className="btns" onClick={() => history.goBack()}>Cancel</button>}
+            </div>
+        </form>
+    )
+}
