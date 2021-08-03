c23f00bc30920f6a42163b6a9057da4a81eaaea8
sxiong0519 <sxiong0519@gmail.com>
2021-08-03 16:44:57 -0500

    added logout feature

--- src/components/nav/NavHome.js
+++ src/components/nav/NavHome.js
@@ -1,10 +1,17 @@
 import React from "react"
-import { Link } from "react-router-dom"
+import { Link, useHistory } from "react-router-dom"
 import "./Nav.css"
 import logo from "./navlogo.png";
 
 export const NavHome = (props) => {
-
+    const history = useHistory()
+    const logout = () => {
+        localStorage.removeItem("autrack_user")
+        }
+    
+        const home = () => {
+            history.push(`/login`)
+        }
 
     return (
         <>
@@ -20,6 +27,11 @@ export const NavHome = (props) => {
             
             <li className="navbar__item">
                 <Link className="navbar__link" to="/locations">Locations</Link>
+            </li>
+            <li className="navbar__item">
+                <Link  className="navbar__link" onClick={() => { 
+                    logout()
+                    home()}}> logout </Link>
             </li>
         </ul>
         </div>
